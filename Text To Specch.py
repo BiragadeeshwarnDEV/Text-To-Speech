0dff6332aa5e7efb24ddc1f9c98821a792ca277a
BiragadeeshwarnDEV <84442817+BiragadeeshwarnDEV@users.noreply.github.com>
2021-05-21 23:28:38 +0530

    Add files via upload

---
+++ Text To Specch.py
@@ -0,0 +1,13 @@
+import pyttsx3
+friend = pyttsx3.init()
+friend.say("Hi Iam Your Speech To Text Conventer")
+friend.say("Iam Made By My Boss Biragadeeshwarn")
+friend.say("Make Sure You Subscribe To His Chanel TechGenix Tamil")
+friend.say("Okay Type What You Want Hear")
+friend.runAndWait()
+speech = input ("Say Something:")
+friend.say(speech)
+friend.say("How Is It")
+friend.say("Did You Know This Was Made By A 10 Year Old Boy")
+friend.say("Bye For now")
+friend.runAndWait()
